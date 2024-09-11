407a2b63a39b111ac4d3747b7f6cfc7973b73f09
shelinadisty <shelinadisty@gmail.com>
2024-09-11 15:24:15 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- keenam.php
+++ keenam.php
@@ -1,49 +1,13 @@
 <?php
-class Author {
-public $name;
-public $description;
-}
-class Publisher {
-    public $name;
-    public $address;
-    public $phone;
 
-    public function setPhone($phone) {
-        $this->phone = $phone;
-    }
-    public function getPhone() {
-        return $this->phone;
-    }
-}
-class Book {
-public $ISBN;
-public $title;
-public $description;
-public $category;
-public $language;
-public $numberOfPage;
-public $author;
-public $publisher;
-public function showAll() {
-    return[
-        'ISBN' => $this-> ISBN,
-        'Title' => $this-> title,
-        'Description' => $this -> description,
-        'Language' => $this -> language,
-        'Number of Page' => $this -> numberOfPage,
-        'Author' => $this -> author,
-        'Publisher' => $this -> publisher,
-    ];
+<?php
 
-}
 
-public function detail($ISBN) {
-    if ($this->ISBN == $ISBN) {
-        return $this->showAll();
-    }
-    return null;
-  }
-}
+require_once "Author";
+require_once "Publisher";
+require_once "Book";
+require_once "Author";
+
 
 $author = new Author();
 $author->name = "F. Scott Fitzgerald";
